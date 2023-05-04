fb12cc7910eda1a3c51a9fde299957a581fb29c6
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-05-04 15:08:50 +0600

    managing players done

--- index.js
+++ index.js
@@ -10,6 +10,45 @@ const io = new Server(server)
 // for using htlm file 
 app.use(express.static(path.resolve("")))
 
+
+// start socket connection
+
+let currentPlayers = []
+let allPlayers = []
+io.on("connection", (socket) =>{
+    console.log("user connected")
+    
+    socket.on("manage-player", e =>{
+        if(e.name != null){
+            currentPlayers.push(e.name)
+
+            if(currentPlayers.length >= 2){
+                let player1 = {
+                    p1name: currentPlayers[0],
+                    p1value: "X",
+                    p1move: ""
+                }
+                let player2 = {
+                    p2name: currentPlayers[1],
+                    p2value: "O",
+                    p2move: ""
+                }
+
+                let playerpair = {
+                    p1: player1,
+                    p2: player2
+                }
+
+                allPlayers.push(playerpair)
+                currentPlayers.splice(0,2)
+
+                io.emit("players", {players: allPlayers})
+            }
+        }
+    })
+})
+
+
 app.get("/", (req, res) =>{
     res.sendFile("index.html")
 })
